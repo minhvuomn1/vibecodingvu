77b1ea41af65e532ddf87fb11d632be9e27ec415
minhvuomn1 <vu.le@omn1solution.com>
2025-10-30 10:48:35 +0700

    test rollback

--- .sfdx/typings/lwc/apex/TodoController.d.ts
+++ .sfdx/typings/lwc/apex/TodoController.d.ts
@@ -1,6 +1,3 @@
 declare module "@salesforce/apex/TodoController.getTodos" {
   export default function getTodos(): Promise<any>;
 }
-declare module "@salesforce/apex/TodoController.markComplete" {
-  export default function markComplete(param: {recordId: any}): Promise<any>;
-}
